58f5dc8c4ea7c46adaf9bf59812a0243387bcdfb
AlexChow <cplover@live.com>
2016-09-14 10:05:13 +0800

    .net core

---
+++ src/ServerControlService/BahamutAppInstanceMonitor.cs
@@ -0,0 +1,77 @@
+﻿using ServerControlService.Model;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerControlService
+{
+    public interface BahamutAppInstanceMonitor
+    {
+        void OnInstanceRegisted(BahamutAppInstanceNotification state);
+        void OnInstanceHeartBeating(BahamutAppInstanceNotification state);
+        void OnInstanceOffline(BahamutAppInstanceNotification state);
+    }
+
+    public class BahamutAppInsanceMonitorManager
+    {
+        static private BahamutAppInsanceMonitorManager _instance = new BahamutAppInsanceMonitorManager();
+        static public BahamutAppInsanceMonitorManager Instance { get { return _instance; } }
+
+        private ConnectionMultiplexer redis;
+        private IDictionary<string,List<BahamutAppInstanceMonitor>> monitors = new Dictionary<string,List<BahamutAppInstanceMonitor>>();
+        
+        public void InitManager(ConnectionMultiplexer redis)
+        {
+            this.redis = redis;
+        }
+
+        public void RegistMonitor(string interestedAppChannel,BahamutAppInstanceMonitor monitor)
+        {
+            try
+            {
+                var list = monitors[interestedAppChannel];
+                if(list.Contains(monitor))
+                {
+                    return;
+                }
+                list.Add(monitor);
+            }
+            catch (Exception)
+            {
+                var list = new List<BahamutAppInstanceMonitor>();
+                list.Add(monitor);
+                monitors[interestedAppChannel] = list;
+            }
+
+            this.redis.GetSubscriber().Subscribe(interestedAppChannel, OnInstanceNotified);
+        }
+
+        private void OnInstanceNotified(RedisChannel channel,RedisValue notification)
+        {
+            try
+            {
+                var list = monitors[channel];
+                var notify = BahamutAppInstanceNotification.FromJson(notification);
+                switch (notify.NotifyType)
+                {
+                    case BahamutAppInstanceNotification.TYPE_INSTANCE_HEART_BEAT:
+                        foreach (var monitor in list) { monitor.OnInstanceHeartBeating(notify); }
+                        break;
+                    case BahamutAppInstanceNotification.TYPE_INSTANCE_OFFLINE:
+                        foreach (var monitor in list) { monitor.OnInstanceOffline(notify); }
+                        break;
+                    case BahamutAppInstanceNotification.TYPE_REGIST_APP_INSTANCE:
+                        foreach (var monitor in list) { monitor.OnInstanceRegisted(notify); }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ src/ServerControlService/BahamutAppInstanceRegister.cs
@@ -0,0 +1,55 @@
+﻿using NLog;
+using ServerControlService.Model;
+using ServerControlService.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerControlService
+{
+    public class BahamutAppInstanceRegister
+    {
+        public static ServerControlManagementService ManagementService { get; private set; }
+
+        public static void RegistAppInstance(ServerControlManagementService ManagementService,BahamutAppInstance appInstance)
+        {
+            if (BahamutAppInstanceRegister.ManagementService == null)
+            {
+                BahamutAppInstanceRegister.ManagementService = ManagementService;
+            }
+            Task.Run(async () =>
+            {
+                try
+                {
+                    await ManagementService.RegistAppInstanceAsync(appInstance);
+                    var observer = ManagementService.StartKeepAlive(appInstance);
+                    observer.OnExpireError += KeepAliveObserver_OnExpireError;
+                    observer.OnExpireOnce += KeepAliveObserver_OnExpireOnce;
+                    LogManager.GetLogger("Main").Info("Bahamut App Instance:" + appInstance.Id.ToString());
+                    LogManager.GetLogger("Main").Info("Keep Server Instance Alive To Server Controller Thread Started!");
+                }
+                catch (Exception ex)
+                {
+                    LogManager.GetLogger("Main").Error(ex, "Unable To Regist App Instance");
+                }
+            });
+        }
+
+        private static void KeepAliveObserver_OnExpireOnce(object sender, KeepAliveObserverEventArgs e)
+        {
+#if DEBUG
+            Console.WriteLine("App Heart Beat : " + e.Instance.Id);
+#endif
+        }
+
+        private static void KeepAliveObserver_OnExpireError(object sender, KeepAliveObserverEventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                LogManager.GetLogger("Main").Error(string.Format("Expire Server Error.Instance:{0}", e.Instance.Id), e);
+                await ManagementService.ReActiveAppInstance(e.Instance);
+            });
+        }
+    }
+}
